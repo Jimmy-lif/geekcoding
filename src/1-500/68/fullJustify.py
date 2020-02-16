fa816915f83a95952a7f0248de5f0e36843566b3
Jimmy-lif <goodlucklf@126.com>
2020-02-16 22:45:23 +0800

    first pass
    first pass the test

--- src/1-500/68/fullJustify.py
+++ src/1-500/68/fullJustify.py
@@ -1,42 +1,51 @@
-# class Solution:
-from typing import List
-def fullJustify(words: List[str], maxWidth: int) -> List[str]:
-    wordLen = 0;
-    result = [];
-    lines = [];
-    str = [];
-    for word in words:
-        if wordLen + len(word) < maxWidth:
-            str.append(word);
-            wordLen += len(word) + 1;
-        else:
-            lines.append(str);
-            str = [word];
-            wordLen = len(word) + 1;
-    for line in lines:
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
         wordLen = 0;
-        for word in line:
-            wordLen += len(word);
-        blankNum = 0;
-        extraNum = 0;
-        if len(line) > 1:
-            blankNum = int((maxWidth - wordLen) / (len(line) - 1));
-            extraNum = maxWidth - blankNum * (len(line) - 1) - wordLen;
-        else:
-            blankNum = maxWidth - wordLen;
+        result = [];
+        lines = [];
+        str = [];
+        for word in words:
+            if wordLen + len(word) <= maxWidth:
+                str.append(word);
+                wordLen += len(word) + 1;
+            else:
+                lines.append(str);
+                str = [word];
+                wordLen = len(word) + 1;
+        lines.append(str);
+        for line in lines[:len(lines) -1]:
+            wordLen = 0;
+            for word in line:
+                wordLen += len(word);
+            blankNum = 0;
+            extraNum = 0;
+            if len(line) > 1:
+                blankNum = int((maxWidth - wordLen) / (len(line) - 1));
+                extraNum = maxWidth - blankNum * (len(line) - 1) - wordLen;
+            else:
+                blankNum = maxWidth - wordLen;
+            outLine = '';
+            for word in line[:len(line) - 1]:
+                outLine += word;
+                for i in range(blankNum):
+                    outLine +=' ';
+                if extraNum > 0:
+                    outLine +=' ';
+                    extraNum -= 1;
+            outLine += line[len(line) - 1];
+            if len(line) == 1:
+                for i in range(blankNum):
+                    outLine +=' ';
+            result.append(outLine);
+
+        lastline = lines[len(lines) - 1];
         outLine = '';
-        for word in line[:len(line) - 1]:
+        for word in lastline[:len(lastline) - 1]:
             outLine += word;
-            for i in range(blankNum):
-                outLine +=' ';
-            if extraNum > 0:
-                outLine +=' ';
-                extraNum -= 1;
-        outLine += line[len(line) - 1];
+            outLine += ' ';
+        outLine += lastline[len(lastline) - 1];
+        for i in range(maxWidth-len(outLine)):
+            outLine += ' ';
         result.append(outLine);
 
-    return result;
-
-
-print(fullJustify(["What","must","be","acknowledgment","shall","be"],16));
-print(fullJustify(["Science","is","what","we","understand","well","enough","to","explain", "to","a","computer.","Art","is","everything","else","we","do"],20));
+        return result;
