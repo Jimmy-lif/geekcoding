7276dcf2475aad3243ec78a944eb3b5839bd609b
Jimmy-lif <goodlucklf@126.com>
2020-02-16 16:06:38 +0800

    Q67 optimize for perf using int(num,2) instead of calc binary self

--- src/1-500/67/addBinary.py
+++ src/1-500/67/addBinary.py
@@ -1,12 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        bnA = 0;
-        for i in range(len(a)):
-            bnA = bnA * 2 + int(a[i]);
-
-        bnB = 0;
-        for i in range(len(b)):
-            bnB = bnB * 2 + int(b[i]);
-
+        bnA = int(a,2)
+        bnB = int(b,2)
         sumAB = bnA + bnB;
         return str(bin(sumAB))[2:];
